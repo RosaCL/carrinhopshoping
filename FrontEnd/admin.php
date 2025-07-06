f0b19d394e68554725ced3014b7c8593dd5c40ff
Rosa <costurezati@gmail.com>
2025-07-06 08:28:24 -0300

    .

--- FrontEnd/admin.php
+++ FrontEnd/admin.php
@@ -5,9 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Costureza (Administrador)</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/FrontEnd/ressources/css/style.css">
+    <link rel="stylesheet" href="./ressources/css/style.css">
 </head>
 <body>
     <?php include 'includes/header.php';?>
+    <div class="container">
+        <section>
+            <form action="" method="post" class="add-product-form" enctype="multipart/form-data">
+                <h3>Adicione um novo produto</h3>
+                <input type="text" name="p_name" id="" placeholder="Nome do produto" required class="box">
+                <input type="number" name="p_price" min="0" placeholder="Preço do produto" required class="box">
+                <input type="file" name="p_image" accept="image/png, image/jpg, image/jpeg"  required class="box" >
+                <input type="submit" name="add_product" value="Adicionar produto" class="btn">
+            </form>
+        </section>
+    </div>
+    <script src="./ressources/js/script.js"></script>
 </body>
 </html>
--- FrontEnd/includes/header.php
+++ FrontEnd/includes/header.php
@@ -1,9 +1,12 @@
-<header>
-        <img src="/FrontEnd/ressources/img/costureza.png" alt="">
-        <nav>
-            <a href="#">Adicionar produtos <i class="fa-solid fa-plus"></i></a>
-            <a href="#">Ver produtos <i class="fa-regular fa-eye"></i></a>
-            <a href="#">Carrinho <i class="fa-solid fa-cart-shopping"></i></a>
-            <a href="https://www.youtube.com/watch?v=MWIUWBWbNV8" target="_blank">Vídeo</a>
-        </nav>
-</header>
+    <header class="header">
+        <div class="flex">
+            <img class="logo" src="/FrontEnd/ressources/img/costureza.png" alt="">
+            <nav class="navbar">
+                <a href="admin.php">Adicionar produtos <i class="fa-solid fa-plus"></i></a>
+                <a href="products.php">Ver produtos <i class="fa-regular fa-eye"></i></a>
+                <a href="https://www.youtube.com/watch?v=MWIUWBWbNV8" target="_blank">Vídeo</a>
+            </nav>
+            <a class="cart" href="cart.php"> <i class="fa-solid fa-cart-shopping"></i> <span>0</span></a>
+            <div class="fas fa-bars" id="menu-btn"></div>
+        </div>
+    </header>
